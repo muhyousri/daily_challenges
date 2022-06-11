f2b06055acd068496c4c3531c863b7599ca48be3
muhyousri <muhyousri@gmail.com>
2022-06-11 13:23:02 +0100

    add challenges description

--- 1-easy-sumwords/main.py
+++ 1-easy-sumwords/main.py
@@ -1,3 +1,16 @@
+"""
+Assign every lowercase letter a value, from 1 for a to 26 for z. Given a string of lowercase letters, find the sum of the values of the letters in the string.
+
+lettersum("") => 0
+lettersum("a") => 1
+lettersum("z") => 26
+lettersum("cab") => 6
+lettersum("excellent") => 100
+lettersum("microspectrophotometries") => 317
+"""
+
+
+
 #main challenge
 def sum_word(word):
     d = {}
